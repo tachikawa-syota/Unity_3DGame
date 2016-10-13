3a588a314af122412fe01f9d9cbbd794c6635279
tachikawa-syota <s.tachikawa1105@gmail.com>
2016-10-13 17:00:24 +0900

    commit

--- Unity_3DGame/Assets/Source/Object/Player/PlayerAction.cs
+++ Unity_3DGame/Assets/Source/Object/Player/PlayerAction.cs
@@ -1,13 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerAction : MonoBehaviour {
+
+public class PlayerAction : MonoBehaviour
+{
 
     // アニメーション
     private Animator m_animation;
+    private Rigidbody m_rigidbody;
     private AnimatorStateInfo m_animatorInfo;
-	private float m_length;
-	private float m_Cur;
 
     public float m_spped = 0.07f;
     public float m_gravity = 0.1f;
@@ -15,45 +16,40 @@ public class PlayerAction : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
+        // コンポーネントを取得
+        m_rigidbody = GetComponent<Rigidbody>();
         m_animation = GetComponent<Animator>();
+        m_animatorInfo = GetComponent<Animator>().GetCurrentAnimatorStateInfo(0);
+
         // 初期アニメーション
         m_animation.Play("Idle");
-        m_animation.SetBool("Walk", false);
-		m_animation.SetBool("Attack", false);
-
-		m_length = m_animatorInfo.length;
-		m_Cur = 0;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-	bool Attack()
-	{
-		m_animatorInfo = GetComponent<Animator>().GetCurrentAnimatorStateInfo(0);
-		m_animation.SetBool ("Attack", true);
-
-		if(m_animatorInfo.normalizedTime >= 1.0f)
-		{
-			m_animation.SetBool ("Attack", false);
-			return true;
-		}
-		return false;
-	}
 
     void FixedUpdate()
     {
         // プレイヤーの向き
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-
+        
         // マウス右クリック
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            if (!Attack())return;
+            m_animation.SetBool("Attack", true);
+            return;
+        }
+        else
+        {
+            m_animation.SetBool("Attack", false);
+        }
+
+        // ジャンプ
+        if(Input.GetKey(KeyCode.Space))
+        {
+            m_animation.SetBool("Jump", true);
+        }
+        else
+        {
+            m_animation.SetBool("Jump", false);
         }
 
         // 入力判定
@@ -66,10 +62,7 @@ public class PlayerAction : MonoBehaviour {
             m_animation.SetBool("Walk", false);
         }
 
-        // 同一のGameObjectが持つRigidbodyコンポーネントを取得
-        Rigidbody rigidbody = GetComponent<Rigidbody>();
-
-       rigidbody.position += new Vector3(x * m_spped, -m_gravity, z * m_spped);
-
+        // 移動
+       m_rigidbody.position += new Vector3(x * m_spped, -m_gravity, z * m_spped);
     }
 }
