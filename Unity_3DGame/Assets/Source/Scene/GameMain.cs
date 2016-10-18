cfa9f69d4356766d6ee6482591c3b4604a621949
tachikawa <s.tachikawa1105@gmail.com>
2016-10-19 00:56:44 +0900

    更新

---
+++ Unity_3DGame/Assets/Source/Scene/GameMain.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMain : MonoBehaviour {
+
+    // 敵(仮)
+    private GameObject m_enemy;
+
+
+	// Use this for initialization
+	void Start () {
+        // 仮
+        m_enemy = Resources.Load<GameObject>("Cube");
+
+        // 動的生成
+        Instantiate(m_enemy, new Vector3(0f, 5f, 0f), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
