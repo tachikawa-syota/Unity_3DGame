23bfc38febe213579ebed85c6f9cd85d7a3f65f5
tachikawa <s.tachikawa1105@gmail.com>
2016-10-24 00:57:38 +0900

    更新

--- Unity_3DGame/Assets/Source/Scene/GameMain.cs
+++ Unity_3DGame/Assets/Source/Scene/GameMain.cs
@@ -6,17 +6,25 @@ public class GameMain : MonoBehaviour {
     // 敵(仮)
     private GameObject m_enemy;
 
-
 	// Use this for initialization
 	void Start () {
         // 仮
         m_enemy = Resources.Load<GameObject>("Cube");
 
         // 動的生成
-        Instantiate(m_enemy, new Vector3(0f, 5f, 0f), Quaternion.identity);
+		Instantiate(m_enemy, new Vector3(0f, 5f, 0f), Quaternion.identity);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.CompareTag("CollisionObject"))
+		{
+			GameObject.Destroy (m_enemy.gameObject);
+			Debug.Log("HIT");
+		}
 	}
 }
