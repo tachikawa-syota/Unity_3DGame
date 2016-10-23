44567a5d66d07e3c5d06033173c6b59ef99aeed8
tachikawa <s.tachikawa1105@gmail.com>
2016-10-24 00:58:03 +0900

    更新

---
+++ Unity_3DGame/Assets/Source/Collision/CollisionEffect.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionEffect : MonoBehaviour 
+{
+    public ParticleSystem m_particle;
+
+	// Use this for initialization
+	void Start () {
+        m_particle = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+  //      if (Time.timeSinceLevelLoad > 10.0f & Time.timeSinceLevelLoad < 50.0f)
+    //    {
+		if (collision.gameObject.CompareTag("CollisionObject"))
+            {
+	//		Destroy (this);
+            //    m_particle.Play(); //パーティクルの再生
+                Debug.Log("HIT");
+            }
+     //   }
+    }
+}
