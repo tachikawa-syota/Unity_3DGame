2c08c36147330f5c2bf69836cfeb804892849590
tachikawa-syota <s.tachikawa1105@gmail.com>
2016-10-26 17:02:37 +0900

    プレイヤープレハブ化

---
+++ Unity_3DGame/Assets/Source/Object/Enemy/EnemyAction.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAction : MonoBehaviour {
+
+    GameObject m_obj;
+
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("OK");
+        if (col.gameObject.CompareTag("CollisionObject"))
+        {
+            Debug.Log("HIT");
+            Destroy(m_obj);
+        }
+    }
+}
